4fc1696b31663a229f9b42a472b4fa1a08866fab
Arian <ariankhadem4@gmail.com>
2025-08-15 22:25:05 +0700

    Enable cors and communicate with frontend

--- oms-backend/src/main.ts
+++ oms-backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const FRONTEND_URL = process.env.FRONTEND_URL
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: FRONTEND_URL,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3001);
+
 }
 bootstrap();
